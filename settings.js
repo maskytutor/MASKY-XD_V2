ed092fdd9075bf50117445445ca90185aa26262f
maskytutor <maskydeploytutor@gmail.com>
2025-09-09 09:51:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "221769421047",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "",
